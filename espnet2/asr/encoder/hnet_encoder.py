e83f2bae4bb9ff60f0f582d81d558e9f6b267111
wutp20 <wutp20@vt.edu>
2025-11-24 19:46:26 -0500

    Updating info

--- espnet2/asr/encoder/hnet_encoder.py
+++ espnet2/asr/encoder/hnet_encoder.py
@@ -14,22 +14,34 @@ from espnet.nets.pytorch_backend.transformer.embedding import (  # noqa: H301
     ConvolutionalPositionalEmbedding,
     PositionalEncoding,
 )
-from hnet_modules.components import ChunkLayer, DeChunkLayer
+from hnet_modules.components import ChunkLayer, DeChunkLayer, RoutingModule
 from hnet_modules.multilayer_perceptron import SwiGLU
-from mamba_ssm import Mamba
+from mamba_ssm import Mamba2
 
 class HNetEncoder(AbsEncoder):
     @typechecked
     def __init__(
         self,
+        dechunk_d_model: int = 256,
+        routing_module_d_model: int = 256,
+        main_network_d_model: int = 256,
+        encoder_d_model: int = 256,
+        encoder_d_state: int = 64,
+        encoder_d_conv: int = 4,
+        encoder_expand: int = 2,
+        decoder_d_model: int = 256,
+        decoder_d_state: int = 64,
+        decoder_d_conv: int = 4,
+        decoder_expand: int = 2,
         device=None,
         dtype=None
     ):
         self.chunk_layer = ChunkLayer()
-        self.dechunk_layer = DeChunkLayer()
-        self.main_network = SwiGLU()
-        self.encoder = Mamba()
-        self.decoder = Mamba()
+        self.dechunk_layer = DeChunkLayer(d_model=dechunk_d_model)
+        self.main_network = torch.nn.Transformer(d_model=main_network_d_model)
+        self.routing_module = RoutingModule(d_model=routing_module_d_model)
+        self.encoder = Mamba2(d_model=encoder_d_model, d_state=encoder_d_state, d_conv=encoder_d_conv, expand=encoder_expand)
+        self.decoder = Mamba2(d_model=decoder_d_model, d_state=decoder_d_state, d_conv=decoder_d_conv, expand=decoder_expand)
 
     def output_size(self) -> int:
         return self._output_size
@@ -47,3 +59,20 @@ class HNetEncoder(AbsEncoder):
             masks = (~make_pad_mask(ilens)[:, None, :]).to(xs_pad.device)
         else:
             masks = ~masks[:, None, :]
+        D = xs_pad.shape[-1]
+        xs_encoded = self.encoder(xs_pad)
+        bpred_output = self.routing_module(
+            xs_encoded,
+            cu_seqlens=ilens,
+            mask=masks,
+        )
+        xs_chunked, next_cu_lens, next_max_seqlen, masks = self.chunk_layer.forward(xs_encoded, bpred_output.boundary_mask)
+        xs_network = self.main_network(xs_chunked)
+        xs_dechunked = self.dechunk_layer(xs_network, bpred_output.boundary_mask, bpred_output.boundary_prob)
+        xs_decoded = self.decoder(xs_dechunked)
+        xs_pad = xs_decoded[..., :D]
+        olens = masks.squeeze(1).sum(1)
+        return xs_pad, olens, None
+
+        
+        
